40333fb9d32ba83d4a0d749ce72fe6b9d8b37e13
moin21 <mamoinuddin@gmail.com>
2022-02-07 21:31:11 +0530

    added:random func dice

--- SnakeAndLadder.java
+++ SnakeAndLadder.java
@@ -1,8 +1,11 @@
 public class SnakeAndLadder {
 
-    public void singlePlayer() {
+    public static void main(String[] args) {
         int position=0;
-
+        int diceRoll = 0;
+        while(position<100) {
+            diceRoll = (int)((Math.random()*10)%6)+1;
+        }
+        }
     }
-    
-}
+
